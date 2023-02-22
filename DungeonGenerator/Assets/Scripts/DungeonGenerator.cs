b672dce63b3c9eabb9b82f6042c9d8bdc5918bf5
DrRon <piepmail@lefthandmedia.com>
2023-02-22 11:22:37 +0100

    Little beginning on Player tile movement

--- DungeonGenerator/Assets/Scripts/DungeonGenerator.cs
+++ DungeonGenerator/Assets/Scripts/DungeonGenerator.cs
@@ -1,4 +1,3 @@
-using SimpleDungeon;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
---
+++ DungeonGenerator/Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using static DungeonGenerator;
+
+public class Player : MonoBehaviour
+{
+    public DungeonGenerator Generator;
+    public Dictionary<Vector2Int, TileType> Kerker = new Dictionary<Vector2Int, TileType>();
+
+    private void Awake()
+    {
+        Generator = GetComponent<DungeonGenerator>();
+        Kerker = Generator.Kerker;
+    }
+
+    public void CanPlayerMove()
+    {
+        foreach (KeyValuePair<Vector2Int, TileType> keyvalue in Kerker)
+        {
+            
+        }
+    }
+}
